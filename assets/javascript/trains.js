47670ce6b66bf4296ef97a7b91fd476c033db9a6
mrduncle1 <dun__can@hotmail.com>
2020-01-04 23:21:31 +0800

    commencement of programming for the firebase database

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -1,6 +1,22 @@
 //@ts-check
 let trainData = {};
 
+  // Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "AIzaSyBuYEqmmv30O-IIBEbaO3UTBRSoBAvn8ao",
+    authDomain: "trainschedule-bd717.firebaseapp.com",
+    databaseURL: "https://trainschedule-bd717.firebaseio.com",
+    projectId: "trainschedule-bd717",
+    storageBucket: "trainschedule-bd717.appspot.com",
+    messagingSenderId: "21166858502",
+    appId: "1:21166858502:web:b6183d5ac7bdfc8ab33e87"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let database = firebase.database();
+
+
 $(document).on("click", "#button", function(event) {
     event.preventDefault();
     trainData.trainName = $("#train-input").val().trim();
@@ -8,6 +24,21 @@ $(document).on("click", "#button", function(event) {
     trainData.startTime = $("#start-time").val().trim();
     trainData.freq = $("#freq").val().trim();
     console.log(trainData);
+})
+
+database.ref().on("value", function(values) {
+    console.log(values.val());
+    console.log(values.val().trainname);
+    console.log(values.val().destination);
+    console.log(values.val().starttime);
+    console.log(values.val().frequency)
+})
+
+database.ref().set({
+    trainname: trainData.trainName,
+    destination: trainData.dest,
+    starttime:trainData.startTime,
+    frequency: trainData.freq
 })
 
 
