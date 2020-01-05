34472014ffbcefa576f347f5ce3f0d8142d3bf51
mrduncle1 <dun__can@hotmail.com>
2020-01-06 05:25:40 +0800

    user entry successfully validated

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -1,5 +1,6 @@
 //@ts-check
 let trainData = {};
+let errorCheck = 0;
 
   // Your web app's Firebase configuration
 let firebaseConfig = {
@@ -18,26 +19,15 @@ let database = firebase.database();
 // console.log("Hello, is anything working?")
 console.log(database);
 
-function checkFreq() {
-    if(isNaN(trainData.freq)) {
-        $("modal-validate").modal("show");
-    }
-}
-
-function checkTime() {
-    let regex = /(?:[01]\d|2[0123]):(?:[012345]\d)/;
-    if (!trainData.startTime.match(regex)) {
-        $("#modal-validate").modal("show");
-    }
-}
-
 function loadFormData() {
+    //extract the data from the form
     trainData.trainName = $("#train-input").val().trim();
     trainData.dest = $("#dest").val().trim();
     trainData.startTime = $("#start-time").val().trim();
     trainData.freq = $("#freq").val().trim();
     console.log(trainData);
-    checkTime();
+
+    //load validated data into the firebase database
     database.ref().push({
         trainname: trainData.trainName,
         destination: trainData.dest,
@@ -46,16 +36,53 @@ function loadFormData() {
     });
 }
 
+function validateEntries() {
+    let modalErrors = "";
+    //check text entries for train name and destination
+    let textCheck = [$("#train-input").val().trim(), $("#dest").val().trim()];
+    $.each(textCheck, function(i, textIn) {
+        if (textIn === "" && i === 0) {
+            modalErrors = "\t\t- Train Name - re-enter a valid train name.\n";
+            errorCheck += 1;
+        }
+        else if (textIn ==="" && i === 1) {
+            modalErrors = modalErrors + "\t\t- Destination - re-enter a valid destination.\n"
+            errorCheck += 1;
+        }
+    })
+
+    //check time supplied is in 24 hour format
+    let timeCheck = $("#start-time").val().trim();
+    //regular expression to validate 24 hour format entry for start time
+    let regex = /(?:[01]\d|2[0123]):(?:[012345]\d)/;
+    if (!timeCheck.match(regex)) {
+        modalErrors = modalErrors + "\t\t- First train time in 24 hour " +
+            "format (hh:mm) - re-enter a valid time.\n";
+        errorCheck += 1;
+    }
+
+    //check train frequency is a number, integer and between 5 and 180 minutes
+    let freqCheck = Number($("#freq").val().trim());
+    // if(isNaN(freqCheck) || !Number.isInteger(freqCheck) || freqCheck > 180 || freqCheck < 5) {
+    if(isNaN(freqCheck) || !Number.isInteger((freqCheck)) || freqCheck > 180 || freqCheck < 5) {
+        modalErrors = modalErrors + "\t\t- Train frequency in whole minutes between 5 and 180 " +
+            "minutes - \t\t\t\t\tre-enter a valid frequency.\n";
+        errorCheck += 1;
+    }
+    $("#modal-message").text("You have not entered valid entries for the " + 
+        "following:\n" + modalErrors);
+    $("#modal-validate").modal("show");
+}
+
 // $("#submit-button").click(function(event) {
-$(document).on("click", "#submit-button, #resubmit-time", function(event) {
-    console.log("Is it getting here?");
+$(document).on("click", "#submit-button", function(event) {
     event.preventDefault();
     if ($(this).attr("id") === "submit-button") {
-        console.log("What about here?");
-        loadFormData();
-    }
-    else if (($this).attr("id") === "resubmit-button") {
-        trainData.startTime = $("#new-time").val().trim();
+        validateEntries();
+        if (errorCheck === 0) {
+            loadFormData();
+        }
+        errorCheck = 0;
     }
 
 //         // checkFreq();
